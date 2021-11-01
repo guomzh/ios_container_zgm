dbf48e68c6e1c30dfbecb3b9b1c153246e76fc3c
zgm <1361097727@qq.com>
2021-11-02 00:41:29 +0800

    image navigate

--- ios_container_zgm/ViewController03.m
+++ ios_container_zgm/ViewController03.m
@@ -14,6 +14,15 @@
 
 @property(nonatomic, assign)int index;
 
+@property (weak, nonatomic) IBOutlet UILabel *indexLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (nonatomic, assign)int allCount;
+
+
 @end
 
 @implementation ViewController03
@@ -21,8 +30,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self pic];
-    self.pic;
+    [self setContentImage:[self pic][_index]];
 }
 
 /*
@@ -40,18 +48,38 @@
     if(_pic == nil) {
         NSString *path = [[NSBundle mainBundle] pathForResource:@"pic.plist" ofType:nil];
         NSArray *array = [NSArray arrayWithContentsOfFile:path];
+        _pic = array;
+        _allCount = (int)array.count;
         NSLog(@"count: %ld", array.count);
     }
     return _pic;
 }
 
 - (IBAction)prePic {
-    
+    self.index--;
+    if(_index == -1) {
+        _index = 2;
+    }
+    NSDictionary *dic = self.pic[self.index];
+    [self setContentImage:dic];
 }
 
 - (IBAction)nextPic {
     self.index++;
-    self.pic[self.index]
+    if(_index == 3) {
+        _index = 0;
+    }
+    NSDictionary *dic = self.pic[self.index];
+    [self setContentImage:dic];
+}
+
+-(void) setContentImage: (NSDictionary *)dic {
+    NSString *title = [dic valueForKey:@"title"];
+    NSString *icon = [dic valueForKey:@"icon"];
+    NSString *index = [NSString stringWithFormat:@"%d/%d",_index+1, _allCount];
+    _imageView.image = [UIImage imageNamed:icon];
+    _indexLabel.text = index;
+    _titleLabel.text = title;
 }
 
 @end
